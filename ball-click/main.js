d9cc9034d70d01b087559d4b1587346a775cc91f
Leandro_SmS <leandrosimoesms@gmail.com>
2023-01-15 17:48:33 -0300

    added remove() to delete div .ball

--- ball-click/main.js
+++ ball-click/main.js
@@ -9,21 +9,6 @@ $(document).ready(() => {
     $('#counter').text(`${count}`);
     $('#level').text(`${level}`);
     $('#round').text(`${round}`);
-
-    //hit the ball logic
-    $('.ball').on('click', (event) => {
-        $(event.currentTarget).css('backgroundColor', 'green').addClass('animate__animated animate__hinge');
-        setTimeout(() => {
-            $(event.currentTarget)
-                .css('visibility', 'hidden')
-                .css('bottom', '0')
-                .css('left', '0')
-                .css('backgroundColor', 'brown')
-                .removeClass('animate__animated animate__hinge animate__backInUp');
-        }, 3000)
-        count ++;
-        $('#counter').text(`${count}`);
-    })
     
 
     //start btn
@@ -42,29 +27,17 @@ $(document).ready(() => {
                         .css('left', `${Math.floor(Math.random() * 90)}%`)
                         .css('bottom', `${Math.floor(Math.random() * 24)}rem`)
                         .addClass('animate__animated animate__backInUp')
-                    );
+                );
                 
-                $('.ball').on('click', (event) => {
+                $(`#ball-${i}`).on('click', (event) => {
                     $(event.currentTarget).css('backgroundColor', 'green').addClass('animate__animated animate__hinge');
                     setTimeout(() => {
-                        $(event.currentTarget)
-                            .css('visibility', 'hidden')
-                            .css('bottom', '0')
-                            .css('left', '0')
-                            .css('backgroundColor', 'brown')
-                            .removeClass('animate__animated animate__hinge animate__backInUp');
+                        $(event.currentTarget).remove();
                     }, 2000)
-                    
-                    $('#counter').text(`${count}`);
                 })
 
                 setTimeout(() => {
-                    $(`#ball-${i}`)
-                    .css('visibility', 'hidden')
-                    .css('bottom', '0')
-                    .css('left', '0')
-                    .css('backgroundColor', 'brown')
-                    .removeClass('animate__animated animate__hinge animate__backInUp');
+                    $(`#ball-${i}`).remove();
                 }, levelTime)
             }, (i * 1000));
         }
