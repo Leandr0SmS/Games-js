54d529e8b782a24f8b919a9d370ab343ba642d26
Leandro_SmS <leandrosimoesms@gmail.com>
2023-01-16 05:47:20 -0300

    added block buttom, show scoreboard; need to improve logic to show scoreboard and block buttom

--- ball-click/main.js
+++ ball-click/main.js
@@ -2,47 +2,58 @@ $(document).ready(() => {
 
     let count = 0;
     let level = 1;
-    let levelTime = 2000;
     let round = 3;
+    let playing = false;
 
     //Statistics
     $('#counter').text(`${count}`);
     $('#level').text(`${level}`);
     $('#round').text(`${round}`);
 
-    //start btn
-    $('#start-btn').on('click', () => { 
-        round --;
-        $('#round').text(`${round}`);
+    //start btn function
+        $('#start-btn').on('click', () => { 
+            
+            playing = true;
+            
+            if (round > 0 ) {
 
-        for (let i = 1; i < 6; i ++) {
-            setTimeout(() => {
-                $('.play-zone')
-                    .append($("<div></div>")
-                    .addClass("ball")
-                    .attr('id', `ball-${i}`)
-                    .append(`${i}`)
-                    .css('visibility', 'visible')
-                    .css('left', `${Math.floor(Math.random() * 90)}%`)
-                    .css('bottom', `${Math.floor(Math.random() * 24)}rem`)
-                    .addClass('animate__animated animate__backInUp')
-                );
-                
-                $(`#ball-${i}`).on('click', (event) => {
-                    $(this).data('clicked', true);
-                    count ++
-                    $('#counter').text(`${count}`);
-                    $(event.currentTarget).css('backgroundColor', 'green').addClass('animate__animated animate__hinge');
+                round --;
+                $('#round').text(`${round}`);
+
+                for (let i = 1; i < 6; i ++) {
                     setTimeout(() => {
-                        $(event.currentTarget).remove();
-                    }, 2000)
-                })
-
-                setTimeout(() => {
-                    $(`#ball-${i}`).remove();
-                }, levelTime)
-            }, (i * 1000));
-        }
+                        $('.play-zone')
+                            .append($("<div></div>")
+                            .addClass("ball")
+                            .attr('id', `ball-${i}`)
+                            .append(`${i}`)
+                            .css('visibility', 'visible')
+                            .css('left', `${Math.floor(Math.random() * 90)}%`)
+                            .css('bottom', `${Math.floor(Math.random() * 24)}rem`)
+                            .addClass('animate__animated animate__backInUp')
+                        );
+                        
+                        $(`#ball-${i}`).on('click', (event) => {
+                            $(this).data('clicked', true);
+                            count ++
+                            $('#counter').text(`${count}`);
+                            $(event.currentTarget).css('backgroundColor', 'green').addClass('animate__animated animate__hinge');
+                            setTimeout(() => {
+                                $(event.currentTarget).remove();
+                            }, 2000)
+                        })
         
-    })
+                        setTimeout(() => {
+                            $(`#ball-${i}`).remove();
+                        }, 4000 - (level * 1000))
+                    }, (i * 1000));
+                }
+            } else {
+                playing = false;
+                level ++
+                $('#level').text(`${level}`);
+                round = 3;
+            }
+            
+        });
 })
