03fd83134448aba06619629a4e10843229e8f115
Leandro_SmS <leandrosimoesms@gmail.com>
2023-01-18 06:59:30 -0300

    added callTheBall function, created a if statement to start click

--- ball-click/main.js
+++ ball-click/main.js
@@ -5,7 +5,15 @@ let level = 1;
 let levelTime = 2000;
 let round = 3;
 
-const welcomeText = 'Hello, This is the Ball Click game, you need to be fast! When you are ready, click at the OK button.';
+const welcomeText = 'Hello, This is the Ball Click game, you need to be fast! Click at the OK button and When you are ready click Start!';
+const score = `Level: ${level} \n Points: ${count}`;
+
+const okBtn = function  () {
+    $('#start-btn')
+        .prop("disabled", false)
+        .css('cursor', 'pointer');
+    $('.infoBoard').css('visibility', 'hidden');
+}
 
 const stopToInfo = function (info) {
     $('#start-btn')
@@ -13,13 +21,40 @@ const stopToInfo = function (info) {
         .css('cursor', 'not-allowed')
     $('.infoBoard').css('visibility', 'visible');
     $('.infoBoard-div').append(info);
+    $('#ok-btn').on('click', () => {
+        okBtn();
+    });
 }
 
-const okBtn = function  () {
-    $('.infoBoard').css('visibility', 'hidden');
-    $('#start-btn')
-        .prop("disabled", false)
-        .css('cursor', 'pointer');
+const callTheBall = function () {
+    for (let i = 1; i < 6; i ++) {
+        setTimeout(() => {
+            $('.play-zone')
+                .append($("<div></div>")
+                .addClass("ball")
+                .attr('id', `ball-${i}`)
+                .append(`${i}`)
+                .css('visibility', 'visible')
+                .css('left', `${Math.floor(Math.random() * 90)}%`)
+                .css('bottom', `${Math.floor(Math.random() * 24)}rem`)
+                .addClass('animate__animated animate__backInUp')
+            );
+            
+            $(`#ball-${i}`).on('click', (event) => {
+                $(this).data('clicked', true);
+                count ++
+                $('#counter').text(`${count}`);
+                $(event.currentTarget).css('backgroundColor', 'green').addClass('animate__animated animate__hinge');
+                setTimeout(() => {
+                    $(event.currentTarget).remove();
+                }, 2000)
+            })
+
+            setTimeout(() => {
+                $(`#ball-${i}`).remove();
+            }, levelTime)
+        }, (i * 1000));
+    }
 }
 
 $(document).ready(() => {
@@ -57,36 +92,14 @@ $(document).ready(() => {
 
     //start btn
     $('#start-btn').on('click', () => { 
+        if (round ==0) {
+            callTheBall()
+            stopToInfo(score);
+        } else {
+            callTheBall()
+        }
         round --;
         $('#round').text(`${round}`);
-
-        for (let i = 1; i < 6; i ++) {
-            setTimeout(() => {
-                $('.play-zone')
-                    .append($("<div></div>")
-                    .addClass("ball")
-                    .attr('id', `ball-${i}`)
-                    .append(`${i}`)
-                    .css('visibility', 'visible')
-                    .css('left', `${Math.floor(Math.random() * 90)}%`)
-                    .css('bottom', `${Math.floor(Math.random() * 24)}rem`)
-                    .addClass('animate__animated animate__backInUp')
-                );
-                
-                $(`#ball-${i}`).on('click', (event) => {
-                    $(this).data('clicked', true);
-                    count ++
-                    $('#counter').text(`${count}`);
-                    $(event.currentTarget).css('backgroundColor', 'green').addClass('animate__animated animate__hinge');
-                    setTimeout(() => {
-                        $(event.currentTarget).remove();
-                    }, 2000)
-                })
-
-                setTimeout(() => {
-                    $(`#ball-${i}`).remove();
-                }, levelTime)
-            }, (i * 1000));
-        } 
     })
+
 })
