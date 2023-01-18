2df64e74e7f4fe02c96b7d8272ce6e136a847b08
Leandro_SmS <leandrosimoesms@gmail.com>
2023-01-18 11:00:51 -0300

    added functions, variables modules; need to export and import

---
+++ ball-click/resources/scripts/_functions.js
@@ -0,0 +1,50 @@
+export const functions = {
+    okBtn () {
+        $('#start-btn')
+            .prop("disabled", false)
+            .css('cursor', 'pointer');
+        $('.infoBoard').css('visibility', 'hidden');
+    },
+    
+    stopToInfo (info) {
+        $('#start-btn')
+            .prop("disabled", true)
+            .css('cursor', 'not-allowed')
+        $('.infoBoard').css('visibility', 'visible');
+        $('.infoBoard-div').append(info);
+        $('#ok-btn').on('click', () => {
+            okBtn();
+        });
+    },
+    
+    callTheBall () {
+        for (let i = 1; i < 6; i ++) {
+            setTimeout(() => {
+                $('.play-zone')
+                    .append($("<div></div>")
+                    .addClass("ball")
+                    .attr('id', `ball-${i}`)
+                    .append(`${i}`)
+                    .css('visibility', 'visible')
+                    .css('left', `${Math.floor(Math.random() * 90)}%`)
+                    .css('bottom', `${Math.floor(Math.random() * 24)}rem`)
+                    .addClass('animate__animated animate__backInUp')
+    );
+                
+                $(`#ball-${i}`).on('click', (event) => {
+                    $(this).data('clicked', true);
+                    count ++
+                    $('#counter').text(`${count}`);
+                    $(event.currentTarget).css('backgroundColor', 'green').addClass('animate__animated animate__hinge');
+                    setTimeout(() => {
+                        $(event.currentTarget).remove();
+                    }, 2000)
+                })
+    
+                setTimeout(() => {
+                    $(`#ball-${i}`).remove();
+                }, levelTime)
+            }, (i * 1000));
+        }
+    }
+}
---
+++ ball-click/resources/scripts/_variables.js
@@ -0,0 +1,7 @@
+let count = 0;
+let level = 1;
+let levelTime = 2000;
+let round = 3;
+
+const welcomeText = 'Hello, This is the Ball Click game, you need to be fast! Click at the OK button and When you are ready click Start!';
+const score = `Level: ${level} \n Points: ${count}`;
