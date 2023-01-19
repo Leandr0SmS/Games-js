b901eb1ec0c9bbeaf5299ab0314c6265c49b5986
Leandro_SmS <leandrosimoesms@gmail.com>
2023-01-19 12:34:36 -0300

    added const variables to variables.js

--- ball-click/main.js
+++ ball-click/main.js
@@ -1,14 +1,11 @@
 import { functions } from "./resources/scripts/functions.js";
-
-let count = 0;
-let level = 0;
-
-const welcomeText = 'Hello, This is the Ball Click game, you need to be fast! Click at the OK button and When you are ready click Start!';
-let endInfo = `Game Over!!`
+import { welcomeText, endInfo } from "./resources/scripts/variables.js"
 
 $(document).ready(() => {
 
     //Statistics
+    let count = 0;
+    let level = 0;
     $('#counter').text(`${count}`);
     $('#level').text(`${level}`);
 
@@ -21,25 +18,14 @@ $(document).ready(() => {
             level ++;
             if (level === 4) {
                 functions.stopToInfo(endInfo);
+                level = 0
+                $('#level').text(`${level}`);
+                count = 0;
+                $('#counter').text(`${count}`);
             } else {
                 functions.callTheBall(count, level);
                 $('#level').text(`${level}`);
             }
         });
     } 
-    //start btn
 });
-
-    /*
-        - function to open infoBoard with insructions
-            (adjust size of infoBoard)
-        -block star buttom
-        - press button of infoBoard to start     
-
-        if (round > 0 $$ level < 3) {
-            -enable button
-            -click-start()
-        } else if () {
-
-        }
-    */
---
+++ ball-click/resources/scripts/variables.js
@@ -0,0 +1,5 @@
+
+const welcomeText = 'Hello, This is the Ball Click game, you need to be fast! Click at the OK button and When you are ready click Start!';
+const endInfo = `Game Over!!`
+
+export { welcomeText, endInfo };
