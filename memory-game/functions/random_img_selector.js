aa5bff62e91054c77c8204434a2edcbd74e5c3a9
Leandro_SmS <leandrosimoesms@gmail.com>
2023-07-17 07:09:00 -0300

    Set random img pick

--- memory-game/functions/random_img_selector.js
+++ memory-game/functions/random_img_selector.js
@@ -1,3 +1,13 @@
-export const imgs_selector = (num) => {
-    
+const imgs_selector = (num) => {
+    const randomNumbers = [];
+    for (let i = 0; randomNumbers.length <= num - 1; i++) {
+        console.log(randomNumbers.length)
+        const randomNum = Math.floor(Math.random() * num) + 1;
+        randomNumbers.includes(randomNum) 
+            ? randomNumbers 
+            : randomNumbers.push(randomNum);
+    }
+    return randomNumbers;
 };
+
+console.log(imgs_selector(10));
